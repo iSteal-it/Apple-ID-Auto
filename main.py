7542670dd27217fe425cf2ce5d838d4630dd5c6b
iSteal-it <47010892+iSteal-it@users.noreply.github.com>
2022-04-24 22:18:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,44 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+import time
 
-capabilities = {
-    "browserName": "chrome",
-    "version": "81.0",
-    "enableVNC": True,
-    "enableVideo": True
-}
+# killallahandallmuslim@pixelpaste.net
+apple_id = "death2allah@isteal-it.com"
+delay = 10
 
-driver = webdriver.Remote(
-    command_executor="http://myserverip:4444/wd/hub",
-    desired_capabilities=capabilities)
+chromedriver: str = "/Users/alyadav/Desktop/chromedriver"
+service = Service(chromedriver)
+options = webdriver.ChromeOptions()
+driver = webdriver.Chrome(service=service, options=options)
 
-driver.get('www.stackoverflow.com')
-print('done')
+while True:
+    driver.get("https://iforgot.apple.com")
+    enter_mail = driver.find_element(by=By.XPATH,
+                                     value="/html/body/div[1]/iforgot-v2/app-container/div/iforgot-body/global-v2/div/idms-flow/div/forgot-password/div/div/div[1]/idms-step/div/div/div/div[2]/div/div[1]/div/div/idms-textbox/idms-error-wrapper/div/div/input")
+    enter_mail.send_keys(apple_id)
+    driver.find_element(by=By.XPATH,
+                        value="/html/body/div[1]/iforgot-v2/app-container/div/iforgot-body/global-v2/div/idms-flow/div/forgot-password/div/div/div[1]/idms-step/div/div/div/div[3]/idms-toolbar/div/div/div/button").click()
+    try:
+        WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CLASS_NAME, 'subtitle')))
+        msg = driver.find_element(by=By.CLASS_NAME, value="subtitle").get_attribute("innerHTML")
+    except NoSuchElementException:
+        WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CLASS_NAME, 'app-title')))
+        msg = driver.find_element(by=By.CLASS_NAME, value="app-title").get_attribute("innerHTML")
+
+    if msg == "Select which information you would like to reset:":
+        print("Apple ID Not Locked")
+        time.sleep(60)
+    elif msg == "Select how you want to unlock your account:":
+        print("Apple ID Locked")
+        driver.find_element(by=By.XPATH,value="/html/body/div[1]/iforgot-v2/app-container/div/iforgot-body/sa/idms-flow/div/section/iforgot-nav/div/div/div[1]/div/div/button[2]").click()
+        time.sleep(5)
+        driver.get("https://mail.hostinger.com")
+
+    elif msg == "Confirm your phone number":
+        print("Apple ID Has 2FA")
+        time.sleep(60)
